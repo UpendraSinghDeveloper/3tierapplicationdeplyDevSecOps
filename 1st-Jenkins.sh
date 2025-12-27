2ab441d845f21ed813307a477e12702acd5b30c2
Upendra singh <71604227+UpendraSinghDeveloper@users.noreply.github.com>
2025-12-27 13:54:44 +0530

    Update 1st-Jenkins.sh

--- 1st-Jenkins.sh
+++ 1st-Jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install JRE
-sudo apt install openjdk-17-jre-headless -y
+sudo apt install openjdk-17-jre-headless -y 
 
 # Create keyrings folder
 sudo mkdir -p /usr/share/keyrings
